bee1828e4afd74b361952f2ada1a309338d59b07
my3v11 <my3v11@yandex.ru>
2014-07-05 12:45:44 +0300

    rotate and translate by click

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ define(function(require, exports, module) {
     var Surface = require("famous/core/Surface");
     var StateModifier = require('famous/modifiers/StateModifier');
     var Transform = require('famous/core/Transform');
+    var Easing = require("famous/transitions/Easing");
 
     var mainContext = Engine.createContext();
 
@@ -17,6 +18,7 @@ define(function(require, exports, module) {
         }
     });
 
+    var updown = 1;
     var translate = new StateModifier({
         origin: [.5,.5], //выполняем трансформацию относительно центра
     })
@@ -30,6 +32,8 @@ define(function(require, exports, module) {
     firstSurface.on("click", function(){
         angle = angle + Math.PI;
         rotate.setTransform(Transform.rotateZ(angle), {duration: 500});      
+        updown = updown * -1;
+        translate.setTransform(Transform.translate(0, 100 * updown, 0), {duration: 500, curve: Easing.inCubic })
     })
 
     mainContext.add(translate).add(rotate).add(firstSurface);
